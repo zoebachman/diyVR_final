d0dd7c855769156f6a99b59fcee846ce16c6b766
Zoe Bachman <zoe.bachman@gmail.com>
2016-11-28 00:10:36 -0500

    first arduino code

---
+++ Unity_Test/Unity_Test.ino
@@ -0,0 +1,29 @@
+//int count = 1000;
+const int analogInPin = A0;
+
+int sensorValue = 0;        // value read from the pot
+
+
+ void setup() {
+   // put your setup code here, to run once:
+   Serial.begin(9600);
+ }
+ 
+ void loop() {
+
+ // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+
+  // change the analog out value:
+//  analogWrite(analogOutPin, outputValue);
+
+  // print the results to the serial monitor:
+//  Serial.print("sensor = ");
+  Serial.println(sensorValue);
+
+  // wait 2 milliseconds before the next loop
+  // for the analog-to-digital converter to settle
+  // after the last reading:
+  delay(500);
+
+ }
