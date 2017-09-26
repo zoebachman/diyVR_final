80c26826b801ec905aed30b253667e52df8d963e
Zoe Bachman <zoe.bachman@gmail.com>
2017-09-26 11:10:38 -0400

    new VR capacitive sensor code

---
+++ vr_capTest.ino
@@ -0,0 +1,44 @@
+
+#include <CapacitiveSensor.h>
+
+/*
+ * CapitiveSense Library Demo Sketch
+ * Paul Badger 2008
+ * Uses a high value resistor e.g. 10M between send pin and receive pin
+ * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
+ * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
+ * Modified by Becky Stern 2013 to change the color of one RGB Neo Pixel based on touch input
+ */
+
+
+CapacitiveSensor   cs_9_10 = CapacitiveSensor(9,10);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor   cs_9_2 = CapacitiveSensor(9,2);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
+//CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+
+int i = 0;
+
+void setup()                    
+{
+   //cs_9_10.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(9600);
+}
+
+void loop()                    
+{
+    long start = millis();
+    long total1 =  cs_9_10.capacitiveSensor(30);
+    long total2 =  cs_9_2.capacitiveSensor(30);
+    //long total3 =  cs_4_8.capacitiveSensor(30);
+
+
+   // Serial.print(millis() - start);        // check on performance in milliseconds
+  // Serial.print("\t");                    // tab character for debug windown spacing
+
+    Serial.print(total1);                  // print sensor output 1
+   Serial.print(",");
+   Serial.println(total2);                  // print sensor output 2
+    //Serial.print("\t");
+    //Serial.println(total3);                // print sensor output 3
+    delay(1000);                             // arbitrary delay to limit data to serial port 
+}
+
